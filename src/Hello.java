89d1c1f18c34986e12162d892d5596356abd12db
Mathew Moreno <mvpmjking@Mathews-MacBook-Air.local>
2021-01-28 20:40:43 -0500

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+    public void sayHello(String name){
+        System.out.println("Hello my name is " + name + "!");
+    }
+
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        hello.sayHello("Mathew");
+    }
+}
